441bf667382513d28776567a2904ddd9ef3cb42c
Masoud <msedighi1377@gmail.com>
2023-08-27 12:34:57 +0330

    Updating Directory component

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,13 @@
+import './Directory.css';
+import CategoryItem from '../category-item/CategoryItem'
+const Directory = ({categories}) => {
+  return (
+    <div className='directory-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
